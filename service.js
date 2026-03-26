8797d375d056b30a11a9921ba2a0a83d412fba75
krystal <kpahinag562@gmail.com>
2026-03-26 16:56:51 +0800

    Create service.js

---
+++ service.js
@@ -0,0 +1,33 @@
+// service.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBBsWGhsw7hHMOGu4QpLEOjNjKCjq_l2a0",
+    authDomain: "fir-ai-app-96845.firebaseapp.com",
+    databaseURL: "https://fir-ai-app-96845-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fir-ai-app-96845",
+    storageBucket: "fir-ai-app-96845.firebasestorage.app", // Naiwan ito sa config pero hindi na natin tatawagin yung service
+    messagingSenderId: "564401363339",
+    appId: "1:564401363339:web:f971caecec0f6f1af5778e"
+};
+
+// Initialize Firebase Apps
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Attach Firebase instances and functions to the window object 
+// so your index.html can access them globally.
+window.fbAuth = auth;
+window.fbDb = db;
+window.fbFunctions = {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    doc,
+    setDoc
+};
+
+console.log("Firebase Auth & Firestore successfully initialized! (Storage handled by Cloudinary)");
